6d8e28628af122f8687c67ce3f9751edb3060ba8
Alemu fikadu <falemu19@gmail.com>
2022-03-22 17:35:33 +0300

    audio

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,75 @@ class Xylophone extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(40),
-                color: Colors.red,
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.yellow,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.orange,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.green,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.teal,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              TextButton(
+                child: Container(
+                  padding: EdgeInsets.all(38),
+                  color: Colors.purple,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
               ),
             ],
           ),
